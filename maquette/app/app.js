7c8f3cb2924adfcce1bec6c974187fb894e0a11a
martin bouchard <martin.bouchard@gsoft-group.com>
2016-01-03 13:52:36 -0500

    gitignore

--- maquette/app/app.js
+++ maquette/app/app.js
@@ -1,4 +1,7 @@
 var application = require("application");
-application.mainModule = "main-page";
+application.mainModule = "login";
 application.cssFile = "./app.css";
+
+var liveedit = require("nativescript-liveedit" );
+
 application.start();
